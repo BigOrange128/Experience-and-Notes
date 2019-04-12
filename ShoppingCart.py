8829c98581b6608e2fe362be6429ed959c47ccc4
BigOrange128 <40637268+BigOrange128@users.noreply.github.com>
2019-04-12 11:26:12 +0800

    Update ShoppingCart.py

--- ShoppingCart.py
+++ ShoppingCart.py
@@ -3,6 +3,8 @@ import time
 user_name = 'brother'
 user_passwd = '7474741'
 
+user = {'brother': 7474741, }
+
 commodity = (
     ('肥仔快乐水', 80), ('肥仔快乐面', 90),
     ('肥仔快乐糖', 60), ('肥仔大礼包', 500),
@@ -10,6 +12,7 @@ commodity = (
 user_money = 0
 user_commodity = []
 while 1:
+    print('测试账号：{}，测试密码：{}'.format(user_name, user_passwd))
     in_name = input("欢迎来到肥仔商店, 请输入您的账号！(退出请输入'quit')\n")
     if in_name  == 'quit':
         print("欢迎下次光临！")
@@ -29,6 +32,7 @@ while 1:
                 print('购物卡金额应为数字,请重新输入!')
                 continue
             while 1:
+                print('序号 商品名 价格')
                 for index, i in enumerate(commodity):
                     print(index, end=' ')
                     for y in i:
@@ -38,6 +42,7 @@ while 1:
                 in_shopping = input("请输入您要购买的商品序号,退出请输入'quit'!\n")
                 if in_shopping == 'quit':
                     print('您的购买清单为:')
+                    print('商品名 价格')
                     for i in user_commodity:
                         for y in i:
                             print(y, end=' ')
@@ -55,6 +60,7 @@ while 1:
                             print('您已成功购买{}！'.format(commodity[in_shopping][0]))
                             user_money = user_money - commodity[in_shopping][1]
                             user_commodity.append(commodity[in_shopping])
+                            print('余额剩余：{}'.format(user_money))
                         else:
                             print('余额不足！')
                     except (ValueError, IndexError) as e:

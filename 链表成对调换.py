a4b33c46261a3bd6416d5d5b15b4058003a1e069
BigOrange128 <40637268+BigOrange128@users.noreply.github.com>
2019-05-26 20:13:36 +0800

    Create 链表成对调换.py

---
+++ 链表成对调换.py
@@ -0,0 +1,35 @@
+'''
+将1->2->3->4转换成2->1->4->3
+'''
+class ListNode:
+    def __init__(self, x, n = None):
+        self.value = x
+        self.next = n
+
+#递归
+# class Solution:
+#     def swapPairs(self, head):
+#         if head != None and head.next != None:
+#             next = head.next
+#             head.next = self.swapPairs(next.next)
+#             next.next = head
+#             return next
+#         return head
+#双指针交换值
+class Solution:
+    def swapPairs(self, head):
+        try:
+            prev = head
+            tail = prev.next
+            while True:
+                prev.value, tail.value = tail.value, prev.value
+                prev = tail.next
+                tail = prev.next
+        finally:
+            return head
+head1 = ListNode(1, ListNode(2, ListNode(3, ListNode(4))))
+
+print(head1.value, head1.next.value, head1.next.next.value, head1.next.next.next.value, head1.next.next.next.next)
+solu = Solution()
+head2 = solu.swapPairs(head1)
+print(head2.value, head2.next.value, head2.next.next.value, head2.next.next.next.value, head2.next.next.next.next)

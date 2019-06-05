14a427326ad949905364b1beca2b5406957db243
BigOrange128 <40637268+BigOrange128@users.noreply.github.com>
2019-06-05 19:47:53 +0800

    Create LinkedListInversion.py

---
+++ LinkedListInversion.py
@@ -0,0 +1,23 @@
+#将单链表逆置
+#1->2->3->4变为1<-2<-3<-4
+class Node(object):
+    def __init__(self, data=None, next=None):
+        self.data = data
+        self.next = next
+
+link = Node(1, Node(2, Node(3, Node(4, Node(5, Node(6, Node(7, Node(8, Node(9)))))))))
+
+def rev(link):
+    pre = link
+    cur = link.next
+    pre.next = None
+    while cur:
+        tmp = cur.next
+        cur.next = pre
+        pre = cur
+        cur = tmp
+    return pre
+root = rev(link)
+while root:
+    print(root.data)
+    root = root.next

52a129a92f45135997da8f8fe7ac0e09c8ec15f2
BigOrange128 <40637268+BigOrange128@users.noreply.github.com>
2019-06-18 11:17:12 +0800

    Update Python_Threading.py

--- Python_Threading.py
+++ Python_Threading.py
@@ -1,5 +1,4 @@
-#函数方式
-
+#函数方法
 import threading
 import time
 
@@ -17,4 +16,24 @@ for t in t_objs:
     t.join()
 print("run time:{}".format(time.time()-start_time))
 
-#类方式
+#类方法
+
+# import threading
+# import time
+#
+# class MyThread(threading.Thread):
+#     def __init__(self, n):
+#         super().__init__()
+#         self.n = n
+#     def run(self):
+#         time.sleep(1)
+#         print('task', self.n)
+# start_time = time.time()
+# t_objs = []
+# for i in range(10):
+#     t = MyThread(i)
+#     t.start()
+#     t_objs.append(t)
+# for t in t_objs:
+#     t.join()
+# print("run time:{}".format(time.time()-start_time))
